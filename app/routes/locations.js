abbba781d90f601ea1749138ccc407fc071c4a68
Richmond Watkins <richmondwatkins@mac.com>
2014-08-18 22:08:25 -0500

    refactor of main location ajax call..also added check for user's active quest

--- app/routes/locations.js
+++ app/routes/locations.js
@@ -19,40 +19,38 @@ exports.index = (req, res)=>{
   res.render('locations/index');
 };
 
-
 exports.getLocations = (req, res)=>{
   var allLocations = {};
-  Location.findAll((locations)=>{ //finds all locations
-    if(res.locals.user){
-      Location.findManyById(res.locals.user.checkIns, allCheckIns=>{ //finds locations that have been completed in user's Active Quest
-        // if there is no active quest, this errors out.
-        Quest.findById(res.locals.user.activeQuest.questId, (err, quest)=>{ //finds users's Active Quest
+  Location.findAll(locations=>{
+    if(res.locals.user.checkIns){
+      Location.findManyById(res.locals.user.checkIns, allCheckIns=>{
+        Quest.findById(res.locals.user.activeQuest.questId, (err, quest)=>{
           Location.removeDuplicates(locations, allCheckIns, allMinusCheckIns=>{
-        if(quest.checkIns.length === res.locals.user.activeQuest.questLocs.length){ //checks if active quest is complete
-            allLocations.all = allMinusCheckIns;
-            allLocations.quest = null;
-            allLocations.checkIns = allCheckIns;
-              res.send(allLocations);
-        }else{
-          Location.findActiveQuestLocations(quest.checkIns, res.locals.user.activeQuest.questLocs, (questLocs)=>{ //finds all locations in the active quest. brings back array of all checkins in quest and completed checkins by user
-            Location.removeDuplicates(allMinusCheckIns, questLocs, allMinusDups=>{ //removes
-              Location.removeDuplicates(allCheckIns, questLocs, allCheckInsMinusDups=>{
-                allLocations.all = allMinusDups;
-                allLocations.quest = questLocs;
-                allLocations.checkIns = allCheckInsMinusDups;
+            if(quest.checkIns.length === res.locals.user.activeQuest.questLocs.length){
+              allLocations.all = allMinusCheckIns;
+              allLocations.quest = null;
+              allLocations.checkIns = allCheckIns;
                 res.send(allLocations);
+            }else{
+              Location.findActiveQuestLocations(quest.checkIns, res.locals.user.activeQuest.questLocs, (questLocs)=>{
+                Location.removeDuplicates(allMinusCheckIns, questLocs, allMinusDups=>{
+                  Location.removeDuplicates(allCheckIns, questLocs, allCheckInsMinusDups=>{
+                    allLocations.all = allMinusDups;
+                    allLocations.quest = questLocs;
+                    allLocations.checkIns = allCheckInsMinusDups;
+                      res.send(allLocations);
+                  });
+                });
               });
-            });
+            }
           });
-         }
         });
-       });
       });
-    }else{
+    } else{
       allLocations.all = locations;
       allLocations.quest = null;
       res.send(allLocations);
-   }
+    }
   });
 };
 
