559174a365f0b1d44b2e7071081ccd41753684b6
ransolo <ransolo@me.com>
2014-09-13 21:40:53 -0500

    added fail test for duplicity on registering a usr also added in "bob" as a before each user

--- test/types/unit/user_spec.js
+++ test/types/unit/user_spec.js
@@ -9,8 +9,7 @@ var expect = require('chai').expect;
 var Mongo = require('mongodb');
 var traceur = require('traceur');
 var db = traceur.require(__dirname + '/../../helpers/db.js');
-// var factory = traceur.require(__dirname + '/../../helpers/factory.js');
-
+var bob;
 var User;
 
 describe('User', function(){
@@ -23,34 +22,34 @@ describe('User', function(){
 
   beforeEach(function(done){
     global.nss.db.collection('users').drop(function(){
-      // factory('user', function(users){
+      User.register({email:'bob@aol.com', password:'123456', nickName:'badass', userName:'bobbydee'}, function(u){
         done();
-      // });
+      });
     });
   });
 
   describe('.register', function(){
     it('should successfully create a user', function(done){
-      User.register({email:'bob@aol.com', password:'123456', nickName:'badass', userName:'ransolo'}, function(u){
+      User.register({email:'ann@aol.com', password:'123456', nickName:'Little Orphan', userName:'Annie'}, function(u){
         User.findById(u._id, function(err, user){
           expect(user).to.be.ok;
           expect(user).to.be.an.instanceof(User);
           expect(user._id).to.be.an.instanceof(Mongo.ObjectID);
-          expect(user.email).to.equal('bob@aol.com');
-          expect(user.nickName).to.equal('badass');
-          expect(user.userName).to.equal('ransolo');
+          expect(user.email).to.equal('ann@aol.com');
+          expect(user.nickName).to.equal('Little Orphan');
+          expect(user.userName).to.equal('Annie');
           expect(user.password).to.have.length(60);
           done();
         });
       });
     });
 
-    // it('should NOT successfully create a user', function(done){
-    //   User.register({email:['sue@aol.com'], password:['does not matter']}, 'ransolo', function(u){
-    //     expect(u).to.be.null;
-    //     done();
-    //   });
-    // });
+    it('should NOT successfully create a user', function(done){
+      User.register({email:'bob@aol.com', password:'123456', nickName:'badass', userName:'bobbdee'}, function(u){
+        expect(User._id).to.be.undefined;
+        done();
+      });
+    });
   });
 
 });
