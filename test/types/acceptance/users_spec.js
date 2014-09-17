40fabaefa4b1f004a90c3f8df71918758c590489
Nathan Hood <nathanhood@me.com>
2014-09-16 19:50:37 -0500

    dashboard and profile acceptance tests passing

--- test/types/acceptance/users_spec.js
+++ test/types/acceptance/users_spec.js
@@ -32,6 +32,44 @@ describe('users', function(){
   });
 
 
+  describe('GET /dashboard - dashboard page', function(){
+    var cookie;
+
+    beforeEach(function(done){
+      request(app)
+      .post('/login')
+      .send('userName=aa')
+      .send('password=aaaaaa')
+      .end(function(err, res){
+        var cookies = res.headers['set-cookie'];
+        var one = cookies[0].split(';')[0];
+        var two = cookies[1].split(';')[0];
+        cookie = one + '; ' + two;
+        done();
+      });
+    });
+
+    it('should successfully load page', function(done){
+      request(app)
+      .get('/dashboard')
+      .set('cookie', cookie)
+      .end(function(err, res){
+        expect(res.status).to.equal(200);
+        done();
+      });
+    });
+
+    it('should not load page - not authorized', function(done){
+      request(app)
+      .get('/dashboard')
+      .end(function(err, res){
+        expect(res.status).to.equal(302);
+        done();
+      });
+    });
+  });
+
+
   describe('GET /users/:userName/checkins - checkins page', function(){
     var cookie;
 
@@ -187,6 +225,45 @@ describe('users', function(){
     it('should not load page - not authorized', function(done){
       request(app)
       .get('/users/edit/' + bob._id)
+      .end(function(err, res){
+        expect(res.status).to.equal(302);
+        done();
+      });
+    });
+  });
+
+
+  describe('GET /users/:userName - profile', function(){
+    var cookie;
+
+    beforeEach(function(done){
+      request(app)
+      .post('/login')
+      .send('userName=aa')
+      .send('password=aaaaaa')
+      .end(function(err, res){
+        var cookies = res.headers['set-cookie'];
+        var one = cookies[0].split(';')[0];
+        var two = cookies[1].split(';')[0];
+        cookie = one + '; ' + two;
+        done();
+      });
+    });
+
+    it('should successfully load page', function(done){
+      request(app)
+      .get('/users/aa')
+      .set('cookie', cookie)
+      .end(function(err, res){
+        expect(res.status).to.equal(200);
+        expect(res.text).to.include('Profile');
+        done();
+      });
+    });
+
+    it('should not load page - not authorized', function(done){
+      request(app)
+      .get('/users/aa')
       .end(function(err, res){
         expect(res.status).to.equal(302);
         done();
